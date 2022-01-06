c28f932773e77863cf80e140ed80ff4cb89b0173
Paul "Rohja" LESELLIER <rohja@rohja.com>
2022-01-06 15:45:23 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,10 @@
-data "template_file" "init" {
+data "template_file" "motd" {
   template = "${file("${path.module}/motd.tpl")}"
   vars = {
     name = "World"
   }
+}
+
+output "motd" {
+  value = data.template_file.motd.rendered
 }
